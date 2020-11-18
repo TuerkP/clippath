222aedfe81836da0f42c3b4589e77d1b4135134f
Philipp Türk <p.tuerk90@gmail.com>
2020-11-18 13:19:36 +0100

    eslint Konfiguration angepasst

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,33 +1,44 @@
+// eslint-disable-next-line no-undef
 module.exports = {
-  extends: [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:react/recommended"
-  ],
-  parser: "@typescript-eslint/parser",
-  plugins: ["@typescript-eslint", "prettier", "react"],
-  settings: {
-    "import/parsers": {
-      "@typescript-eslint/parser": [".ts", ".tsx"]
+    extends: [
+      "eslint:recommended",
+      "plugin:@typescript-eslint/recommended",
+      "plugin:react-hooks/recommended",
+      "plugin:react/recommended",
+    ],
+    parser: "@typescript-eslint/parser",
+    plugins: ["react-hooks", "@typescript-eslint", "prettier", "react"],
+    settings: {
+      "import/parsers": {
+        "@typescript-eslint/parser": [".ts", ".tsx"],
+      },
+      "import/resolver": {
+        typescript: {},
+      },
+      react: {
+        version: "detect",
+      },
     },
-    "import/resolver": {
-      typescript: {}
+    rules: {
+      "max-len": ["error", {code: 100}],
+      "operator-linebreak": ["error", "before"],
+      "array-bracket-spacing": ["error", "never"],
+      "object-curly-spacing": ["error", "never"],
+      "@typescript-eslint/explicit-function-return-type": "off",
+      "@typescript-eslint/explicit-module-boundary-types": "off",
+      "@typescript-eslint/naming-convention": [
+        "error",
+        {selector: "variable", format: ["camelCase", "UPPER_CASE"]},
+        {selector: "function", format: ["PascalCase"]},
+        {
+          selector: "parameter",
+          format: ["camelCase"],
+          filter: {
+            regex: "^__",
+            match: false,
+          },
+        },
+        {selector: "interface", format: ["PascalCase"], custom: {regex: "^I[A-Z]", match: false}},
+      ],
     },
-    react: {
-      version: "detect"
-    }
-  },
-  env: {
-    browser: true
-  },
-  rules: {
-    "max-len": ["error", { code: 100 }],
-    "operator-linebreak": ["error", "before"],
-    "array-bracket-spacing": ["error", "never"],
-    "object-curly-spacing": ["error", "never"],
-    "react/jsx-filename-extension": [2, { extensions: [".js", ".jsx", ".ts", ".tsx"] }],
-    "@typescript-eslint/indent": [2, 2],
-    "@typescript-eslint/explicit-function-return-type": "off",
-    "@typescript-eslint/explicit-module-boundary-types": "off"
-  }
-};
+  };
