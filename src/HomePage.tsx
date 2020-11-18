b107afe6738bf826e1d5c9ccdd12b8615f923a3e
Philipp Türk <p.tuerk90@gmail.com>
2020-11-18 13:20:58 +0100

    Homepage in FC umgebaut und Menu Komponente hinzugefügt.

--- src/HomePage.tsx
+++ src/HomePage.tsx
@@ -1,137 +1,173 @@
-import {Button, createStyles, withStyles, WithStyles} from "@material-ui/core";
-import React, {Component} from "react";
+import {Button, makeStyles} from "@material-ui/core";
+import DeleteIcon from "@material-ui/icons/Delete";
+import EditIcon from "@material-ui/icons/Edit";
+import React, {useRef, useState} from "react";
 import ClipPathBuilder, {Point} from "./ClipPathBuilder";
+import Menu, {MenuItem} from "./Menu";
 
-const styles = () =>
-  createStyles({
-    root: {
-      width: "100vw",
-      height: "100vh",
+const useStyles = makeStyles({
+  root: {
+    width: "100vw",
+    height: "100vh",
+  },
+  clipPathBilderContainer: {
+    display: "flex",
+    justifyContent: "center",
+    marginTop: "10px",
+  },
+  buttonContainer: {
+    display: "flex",
+    justifyContent: "center",
+    marginTop: "10px",
+  },
+  button: {
+    marginRight: "10px",
+    "&:last-child": {
+      marginRight: "0px",
     },
-    clipPathBilderContainer: {
-      display: "flex",
-      justifyContent: "center",
-      marginTop: "10px",
+  },
+  previewContainer: {
+    marginTop: "10px",
+    display: "flex",
+    justifyContent: "center",
+    position: "relative",
+  },
+  preview: {
+    border: "1px solid black",
+    position: "relative",
+    width: "fit-content",
+    height: "fit-content",
+  },
+  clickArea: {
+    width: "100%",
+    height: "100%",
+    top: 0,
+    left: 0,
+    position: "absolute",
+    display: "block",
+    cursor: "pointer",
+    "&:hover": {
+      background: "white",
+      opacity: 0.4,
     },
-    buttonContainer: {
-      display: "flex",
-      justifyContent: "center",
-      marginTop: "10px",
-    },
-    button: {
-      marginRight: "10px",
-      "&:last-child": {
-        marginRight: "0px",
-      },
-    },
-    previewContainer: {
-      marginTop: "10px",
-      display: "flex",
-      justifyContent: "center",
-    },
-    preview: {
-      border: "1px solid black",
-      position: "relative",
-      width: "fit-content",
-      height: "fit-content",
-    },
-    clickArea: {
-      width: "100%",
-      height: "100%",
-      top: 0,
-      left: 0,
-      position: "absolute",
-      display: "block",
-      cursor: "pointer",
-      "&:hover": {
-        background: "white",
-        opacity: 0.4,
-      },
-    },
-  });
+  },
+});
 
-type Props = WithStyles<typeof styles>;
-
-interface State {
-  points: Point[];
-  zoom: number;
-  savedPoints: Point[][];
+interface Position {
+  x: number;
+  y: number;
 }
 
-class HomePage extends Component<Props, State> {
-  public readonly state: State = {points: [], zoom: 1, savedPoints: []};
+function HomePage() {
+  const classes = useStyles();
 
-  private pointToStr = (point: Point) => `${point.left}% ${point.top}%`;
+  const previewRef = useRef() as React.MutableRefObject<HTMLDivElement>;
 
-  private onChange = (points: Point[]) => this.setState({points: points});
+  const [points, setPoints] = useState<Point[]>([]);
+  const [savedPoints, setSavedPoints] = useState<Point[][]>([]);
+  const [zoom, setZoom] = useState(1);
+  const [previewPos, setPreviewPos] = useState<Position>({x: 0, y: 0});
+  const [areaIdx, setAreaIdx] = useState<number | undefined>(undefined);
 
-  private onSave = () => {
-    const points = [...this.state.points];
-    this.setState({savedPoints: [...this.state.savedPoints, points], points: []});
+  const pointToStr = (point: Point) => `${point.left}% ${point.top}%`;
+
+  const onChange = (points: Point[]) => setPoints(points);
+
+  const onSave = () => {
+    setSavedPoints([...savedPoints, points]);
+    setPoints([]);
   };
 
-  private onDelete = () => this.setState({points: []});
+  const onDelete = () => setPoints([]);
 
-  private onZoomIn = () => this.setState({zoom: this.state.zoom + 0.2});
+  const onZoomIn = () => setZoom(zoom + 0.2);
 
-  private onZoomOut = () => this.setState({zoom: this.state.zoom - 0.2});
+  const onZoomOut = () => setZoom(zoom - 0.2);
 
-  private onZoomReset = () => this.setState({zoom: 1});
+  const onZoomReset = () => setZoom(1);
 
-  render() {
-    const {classes} = this.props;
-    const {points, zoom} = this.state;
+  const onEditArea = () => {
+    if (areaIdx !== undefined) {
+      setPoints(savedPoints[areaIdx]);
+      setSavedPoints(savedPoints.filter((__, idx) => idx !== areaIdx));
+      hideMenu();
+    }
+  };
 
-    return (
-      <div className={classes.root}>
-        <div className={classes.clipPathBilderContainer}>
-          <ClipPathBuilder
-            src=""
-            alt="editor"
-            points={points}
-            zoom={zoom}
-            onChange={this.onChange}
-          />
+  const onDeleteArea = () => {
+    if (areaIdx !== undefined) {
+      setSavedPoints(savedPoints.filter((__, idx) => idx !== areaIdx));
+      hideMenu();
+    }
+  };
+
+  const showMenu = (areaIdx: number) => (event: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+    if (previewRef.current) {
+      const rect = previewRef.current.getBoundingClientRect();
+      const x = event.clientX - rect.left;
+      const y = event.clientY - rect.top;
+      setPreviewPos({x, y});
+      setAreaIdx(areaIdx);
+    }
+  };
+
+  const hideMenu = () => setAreaIdx(undefined);
+
+  return (
+    <div className={classes.root}>
+      <div className={classes.clipPathBilderContainer}>
+        <ClipPathBuilder
+          src="https://www.prlog.org/11283630-prehbmw-idrive-controller.jpg"
+          alt="editor"
+          points={points}
+          zoom={zoom}
+          onChange={onChange}
+        />
+      </div>
+      <div>
+        <div className={classes.buttonContainer}>
+          <Button className={classes.button} variant="outlined" onClick={onZoomIn}>
+            +
+          </Button>
+          <Button className={classes.button} variant="outlined" onClick={onZoomReset}>
+            0
+          </Button>
+          <Button className={classes.button} variant="outlined" onClick={onZoomOut}>
+            -
+          </Button>
         </div>
-        <div>
-          <div className={classes.buttonContainer}>
-            <Button className={classes.button} variant="outlined" onClick={this.onZoomIn}>
-              +
-            </Button>
-            <Button className={classes.button} variant="outlined" onClick={this.onZoomReset}>
-              0
-            </Button>
-            <Button className={classes.button} variant="outlined" onClick={this.onZoomOut}>
-              -
-            </Button>
-          </div>
-          <div className={classes.buttonContainer}>
-            <Button className={classes.button} variant="outlined" onClick={this.onSave}>
-              Speichern
-            </Button>
-            <Button className={classes.button} variant="outlined" onClick={this.onDelete}>
-              Löschen
-            </Button>
-          </div>
-        </div>
-        <div className={classes.previewContainer}>
-          <div className={classes.preview}>
-            <img
-              src=""
-              alt="preview"
-            />
-            {this.state.savedPoints.map((points, idx) => (
-              <div
-                key={idx}
-                style={{clipPath: `polygon(${points.map(this.pointToStr).join(", ")})`}}
-                className={classes.clickArea}
-              />
-            ))}
-          </div>
+        <div className={classes.buttonContainer}>
+          <Button className={classes.button} variant="outlined" onClick={onSave}>
+            Speichern
+          </Button>
+          <Button className={classes.button} variant="outlined" onClick={onDelete}>
+            Löschen
+          </Button>
         </div>
       </div>
-    );
-  }
+      <div className={classes.previewContainer}>
+        <div ref={previewRef} className={classes.preview}>
+          <img src="https://www.prlog.org/11283630-prehbmw-idrive-controller.jpg" alt="preview" />
+          {savedPoints.map((points, areaIdx) => (
+            <div
+              key={areaIdx}
+              style={{clipPath: `polygon(${points.map(pointToStr).join(", ")})`}}
+              className={classes.clickArea}
+              onClick={showMenu(areaIdx)}
+            />
+          ))}
+          <Menu open={areaIdx !== undefined} relativePosition={previewPos} onClose={hideMenu}>
+            <MenuItem onClick={onEditArea} icon={<EditIcon />}>
+              Bearbeiten
+            </MenuItem>
+            <MenuItem onClick={onDeleteArea} icon={<DeleteIcon />}>
+              Löschen
+            </MenuItem>
+          </Menu>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default withStyles(styles)(HomePage);
+export default HomePage;
---
+++ src/Menu.tsx
@@ -0,0 +1,67 @@
+import {Button, ClickAwayListener, makeStyles} from "@material-ui/core";
+import React from "react";
+
+interface Position {
+  x: number;
+  y: number;
+}
+
+interface StyleProps {
+  pos: Position;
+}
+
+const useMenuStyles = makeStyles({
+  container: (props: StyleProps) => ({
+    position: "absolute",
+    top: props.pos.y,
+    left: props.pos.x,
+    backgroundColor: "#FFFFFF",
+    borderRadius: "4px",
+    display: "flex",
+    flexDirection: "column",
+  }),
+});
+
+const useMenuItemStyles = makeStyles({
+  menuItem: {
+    padding: "10px",
+    borderRadius: "0px",
+    justifyContent: "left",
+  },
+});
+
+interface Props {
+  open: boolean;
+  relativePosition: Position;
+  onClose: () => void;
+  children?: React.ReactNode[];
+}
+
+export default function Menu(props: Props) {
+  const classes = useMenuStyles({pos: props.relativePosition});
+  return props.open ? (
+    <ClickAwayListener onClickAway={props.onClose}>
+      <div className={classes.container}>{props.children}</div>
+    </ClickAwayListener>
+  ) : null;
+}
+
+interface MenuItemProps {
+  icon: React.ReactNode;
+  children: string;
+  onClick?: (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void;
+}
+
+export function MenuItem(props: MenuItemProps) {
+  const classes = useMenuItemStyles();
+  return (
+    <Button
+      onClick={props.onClick}
+      startIcon={props.icon}
+      fullWidth={true}
+      className={classes.menuItem}
+    >
+      {props.children}
+    </Button>
+  );
+}
